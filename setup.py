5aceca48e861dc3221d5cc5c7b00eb9b30194ed6
camille-004 <dunningcamille@gmail.com>
2022-04-17 02:54:46 -0700

    Pre-commit works

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='project_name',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Short description of project',
-    author='author_name',
-    author_email='author@email.com',
-    license='license_type',
+    version="0.1.0",
+    description="Short description of project",
+    author="author_name",
+    author_email="author@email.com",
+    license="license_type",
 )
