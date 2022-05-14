25cb94816e617c43b0c9ce30c8b9687db1097d8e
camille-004 <dunningcamille@gmail.com>
2022-05-14 16:12:51 -0700

    Start docs, logging

--- src/logger/logging.py
+++ src/logger/logging.py
@@ -8,6 +8,13 @@ APP_LOGGER_NAME = "data-science-boilerplate"
 def setup_app_level_logger(
     logger_name: str = APP_LOGGER_NAME, file_name: str = None
 ) -> logging.Logger:
+    """
+    Initialize root app-level logger.
+
+    :param logger_name: The name of the app-level logger.
+    :param file_name: The name of the file in which to store all log messages.
+    :return: Return the logger.
+    """
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
 
@@ -27,3 +34,13 @@ def setup_app_level_logger(
         logger.addHandler(fh)
 
     return logger
+
+
+def get_logger(module_name: str) -> logging.Logger:
+    """
+    Make sure our modules can call the logger when needed.
+
+    :param module_name: Module that will get this logger.
+    :return: Return the logger.
+    """
+    return logging.getLogger(APP_LOGGER_NAME).getChild(module_name)
